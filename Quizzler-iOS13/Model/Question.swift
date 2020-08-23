083f3a647b83c624bfbd6c667c01ae8094bc8fae
Elena Vigo <elenavigoolivan@gmail.com>
2020-08-23 14:29:30 +0200

    first version of ios quizz app

---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,19 @@
+//
+//  question.swift
+//  Quizzler-iOS13
+//
+//  Created by Elena Vigo Olivan on 22/08/2020.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+}
